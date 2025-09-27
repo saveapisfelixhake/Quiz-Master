cea238c0eef75630c20896e3a1bd62f4649a8a00
Raion <85371175+RaionReworld@users.noreply.github.com>
2025-09-27 15:21:55 +0200

    fix: Add missing migration

---
+++ src/Backend/Domains/Common/Persistence/Sql/Migrations/20250927132126_AddMissingQuizAttributes.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Domains.Common.Persistence.Sql.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMissingQuizAttributes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "QuizState",
+                table: "Quizzes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAnswered",
+                table: "Questions",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "QuizState",
+                table: "Quizzes");
+
+            migrationBuilder.DropColumn(
+                name: "IsAnswered",
+                table: "Questions");
+        }
+    }
+}
